48815e165726169a5457d60321fa11950aca83d2
Lionheart0305 <shivam.srivastava@lntinfotech.com>
2020-08-06 12:39:09 +0530

    product function added

--- EmployeeManagementSystem/src/com/lti/Calculator.java
+++ EmployeeManagementSystem/src/com/lti/Calculator.java
@@ -9,10 +9,15 @@ public class Calculator {
 	public static void diff(int a,int b) {
 		System.out.println("Difference= "+(a-b));
 	}
+	
+	public static void prod(int a,int b) {
+		System.out.println("Difference= "+(a*b));
+	}
 
 	public static void main(String[] args) {
 		add(10,30);
 		diff(30,10);
+		prod(2, 5);
 	}
 
 }
